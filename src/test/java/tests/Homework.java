a325f5bd1dd96032833e3a41a80c5db67c238538
Ivan <sidorenko.evan@yandex.ru>
2022-08-28 19:29:59 +0300

    homework's done

---
+++ src/test/java/tests/Homework.java
@@ -0,0 +1,82 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class Homework {
+    @Test
+    void listUsers() {
+
+        given()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("page", is(2))
+                .body("per_page", is(6))
+                .body("total", is(12))
+                .body("total_pages", is(2));
+    }
+
+    @Test
+    void createUser() {
+        String body = "{\"name\": \"morpheus\",     \"job\": \"leader\" }";
+        given()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void updateUser() {
+        String body = "{\"name\": \"morpheus\",\"job\": \"zion resident\"}";
+        given()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name",is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void deleteUser() {
+        given()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    void registerSuccesfull() {
+        String body = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+        given()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id",is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+}
+
